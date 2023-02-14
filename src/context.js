ec8388b9aabb4483917686569b776567c30dbe1c
rajat4984 <rs20021023@gmail.com>
2023-02-14 23:49:16 +0530

    fetched anime array from context

--- src/context.js
+++ src/context.js
@@ -7,7 +7,7 @@ const AppContext = React.createContext();
 export const AppProvider = ({ children }) => {
   const getData = async () => {
     const url =
-      "https://api.themoviedb.org/3/discover/tv?api_key=292b9360de2fad72bc11fb6a4f8362f4&with_genres=16&page=2";
+      "https://api.themoviedb.org/3/discover/tv?api_key=292b9360de2fad72bc11fb6a4f8362f4&with_genres=16&page=3";
     const response = await axios.get(url);
     const data = await response.data;
     return data; //this contains all animation including other languages
